91d868d6bc3c5cbaa5ed60d1546f7051b7876023
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-11-17 14:21:57 +0800

    react tailwind setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import "./App.css";
-
-function App() {
+const App = () => {
   return (
-    <>
-      <h1>Test</h1>
-    </>
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">Hello World</div>
   );
-}
+};
 
 export default App;
